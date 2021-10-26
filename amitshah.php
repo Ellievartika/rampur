660cc5c92624d359e75a0c38c05a4ccfde7633b1
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:52:37 +0530

    amitshah files

---
+++ amitshah.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>श्री अमित शाह - भारतीय जनता पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <!-- <link href="assets/img/" rel="icon"> -->
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- navheading css -->
+    <link rel="stylesheet" href="assets/css/navheading-partykebareme.css">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <div class="">
+        <img src="assets/img/headerimages/amitshah.jpg" class="img-fluid" alt="">
+    </div>
+
+     <!-- navbar for heading -->
+     <div class="" style="height: 60px; background-color:green; text-align:center; color:orange;">
+        <div class="navheading">
+            <h4 data-aos="fade-down">श्री अमित शाह</h4>
+        </div>
+    </div>
+
+    <!-- ======= About Us Section ======= -->
+    <section style="margin-left: 0%;" class="content">
+        <div class="container">
+            <div class="row">
+                <h4>श्री अमित शाह के बारे में</h4>
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        22 अक्टूबर 1964 को मुंबई में उनका जन्म हुआ था। 16 वर्ष की उम्र तक उन्होंने अपनी
+                        शिक्षा अपने ननिहाल गुजरात के मनसा में ग्रहण की जिसके बाद उनका परिवार गुजरात बस गया।
+                        बचपन से ही उन्हें देश के लिए प्राणों की आहुति देने वाले नायकों की गौरव गाथाएं बेहद
+                        प्रिय थीं और वह हमेशा ही देश के लिए भी समर्पित होकर कुछ करना चाहते थे। वह विशेष तौर
+                        पर राष्ट्रीय स्वयंसेवक संघ से बेहद प्रभावित थे और अहमदाबाद में ही सक्रिय रूप से संघ
+                        में शामिल हो गए थे। इस एक कदम से उनके जीवन की दिशा ही बदल गई और वह भारतीय जनता
+                        पार्टी के नेतृत्व में आगे बढ़ने लगे।
+                    </p>
+                </div>
+                <div class="text-center btnnarendra mt-4">
+                    <div class="btnn">
+                        <a type="submit" href="http://amitshah.co.in/" class="btn btn-lg">और पढ़े </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div><!-- End .content-->
+        </div>
+        </div>
+    </section><!-- End About Us Section -->
+
+
+    </main><!-- End #main -->
+
+    <?php include('footer.php') ?>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
