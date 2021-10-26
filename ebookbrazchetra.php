920c6d19daedda5148a81ab9453150710206dbd3
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:57:01 +0530

    files

---
+++ ebookbrazchetra.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>E-बुक : ब्रज क्षेत्र - भारतीय जनता पार्टी</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <main id="main">
+        <!-- ======= Section ======= -->
+        <section id="pricing" class="pricing">
+            <div class="container-fluid">
+
+                <div class="text-center mb-4" data-aos="fade-down" style="background-color: #f3f1f0; height: 40px;">
+                    <div class="kamaljyotiheading">
+                        <h2>
+                            E-बुक : ब्रज क्षेत्र
+                        </h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    जिला आगरा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/agra-jila-new-build_0001-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    आगरा महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/agra-mahanagar_0001-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    जिला अलीगढ़
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Aligarh-Jila_0001-208x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    अलीगढ़ महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Aligarh-Mahanagar_0001-208x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    जिला आंवला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Avla_0001-208x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बरेली
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Bareli-jila_0001-212x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बरेली महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/bareli-mahanagar_0001-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बदायूँ
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/budaun_0001-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    एटा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/etah_0001-208x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    फिरोजाबाद 
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Firozabad-Jila_0001-208x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    फिरोजाबाद महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/firozabad-mahanagar-final-ok_0001-208x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    हाथरस
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Hathras-हाथरस_0001-208x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    कासगंज
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Kasganj_0001-208x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मैनपुरी
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Mainpuri_0001-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मथुरा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Mathura-jila_0001-208x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मथुरा महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Mathura-Mahanagar_0001-208x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    पीलीभीत
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/PILIBHIT_0001-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    शाहजहॉंपुर 
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Shahjahanpur-Jila_0001-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    शाहजहॉंपुर महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookbrezchetra/Shahjahanpur-Mahanagr_0001-208x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- End Section -->
+
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include('footer.php') ?>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
