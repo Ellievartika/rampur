2cd605f56c25b49d43fb39cd47f65d1e2909ed96
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:07:59 +0530

    files

---
+++ panditdindyal.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>पंडित दीनदयाल उपाध्याय - भारतीय जनता पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <!-- <link href="assets/img/" rel="icon"> -->
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- after image green nav css -->
+    <link rel="stylesheet" href="assets/css/navheading-partykebareme.css">
+
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <div class="">
+        <img src="assets/img/headerimages/website-cover-pandit-deendayal-upadhyayh-1.webp" class="img-fluid" alt="">
+    </div>
+
+    <!-- navbar for heading -->
+    <div class="" style="height: 60px; background-color:green; text-align:center; color:orange;">
+        <div class="navheading">
+            <h4 data-aos="fade-down"> संक्षिप्त जीवन परिचय</h4>
+        </div>
+    </div>
+    <!-- end heading navbar -->
+
+    <!-- ======= About Us Section ======= -->
+    <section style="margin-left: 0%;" class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        पंडित दीनदयाल उपाध्याय का जन्म 25 सितंबर, 1916 को उत्तर प्रदेश की पवित्र ब्रजभूमि
+                        मथुरा में नगला चंद्रभान नामक गांव में हुआ था। बचपन में एक ज्योतिषी ने इनकी
+                        जन्मकुंडली देख कर भविष्यवाणी की थी कि आगे चलकर यह बालक एक महान विद्वान एवं विचारक
+                        बनेगा, एक अग्रणी राजनेता और नि:स्वार्थ सेवाव्रती होगा मगर ये विवाह नहीं करेगा। बचपन
+                        में ही दीनदयाल जी को एक गहरा आघात सहना पड़ा जब सन 1934 में बीमारी के कारण उनके भाई की
+                        असामयिक मृत्यु हो गयी। उन्होंने अपनी हाई स्कूल की शिक्षा वर्त्तमान राजस्थान के सीकर
+                        में प्राप्त की। विद्याध्ययन में उत्कृष्ट होने के कारण सीकर के तत्कालीन नरेश ने बालक
+                        दीनदयाल को एक स्वर्ण पदक, किताबों के लिए 250 रुपये और दस रुपये की मासिक छात्रवृत्ति
+                        से पुरस्कृत किया |
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        दीनदयाल जी ने अपनी इंटरमीडिएट की परीक्षा पिलानी में विशेष योग्यता के साथ उत्तीर्ण
+                        की। तत्पश्चात वो बीए. की शिक्षा ग्रहण करने के लिए कानपूर आ गए जहां उन्होंने सनातन
+                        धर्म कॉलेज में दाखिला लिया। अपने एक मित्र श्री बलवंत महाशब्दे की प्रेरणा से सन 1937
+                        में वो राष्ट्रीय स्वयंसेवक संघ में सम्मिलित हो गए। उसी वर्ष उन्होंने बीए. की परीक्षा
+                        भी प्रथम श्रेणी में उत्तीर्ण की। इसके बाद एमए. की पढ़ाई के लिए वो आगरा आ गए।
+                    </p>
+                    <p data-aos="fade-up">
+                        आगरा में राष्ट्रीय स्वयंसेवक संघ की सेवा के दौरान उनका परिचय श्री नानाजी देशमुख और
+                        श्री भाउ जुगडे से हुआ। इसी समय दीनदयाल जी की बहन सुश्री रमादेवी बीमार पड़ गयीं और
+                        इलाज के लिए आगरा आ गयीं। मगर दुर्भाग्यवश उनकी मृत्यु हो गयी। दीनदयाल जी के लिए जीवन
+                        का यह दूसरा बड़ा आघात था। इसके कारण वह अपनी एमए. की परीक्षा नहीं दे सके और उनकी
+                        छात्रवृत्ति भी समाप्त हो गयी।
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        अपनी चाचीजी के कहने पर दीनदयालजी एक सरकारी प्रतियोगितात्मक परीक्षा में सम्मिलित हुए।
+                        इस परीक्षा में वो धोती और कुरता पहने हुए थे और अपने सर पर टोपी लगाए हुए थे। अन्य
+                        परीक्षार्थी पश्चिमी ढंग के सूट पहने हुए थे। मज़ाक में उनके साथियों ने उन्हें “पंडित
+                        जी” कह कर पुकारना शुरू कर दिया, आगे चलकर उनके लाखों प्रशंसक और अनुयायी आदर और प्रेम
+                        से उन्हें इसी नाम से पुकारने वाले थे। इस परीक्षा में भी उन्होंने प्रथम स्थान प्राप्त
+                        किया।
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        पंडित दीनदयाल जी इसके बाद बीटी. का कोर्स करने के लिए प्रयागराज (इलाहाबाद) आ गए और
+                        यहां पर भी राष्ट्रीय स्वयंसेवक संघ की सेवा जारी रखी। बीटी. का कोर्स पूरा करने के बाद
+                        वो राष्ट्रीय स्वयंसेवक संघ में पूर्णकालिक रूप से समर्पित हो गए और संगठक के रूप में
+                        उत्तर प्रदेश के लखीमपुर ज़िले में आ गए। सन 1955 में वो राष्ट्रीय स्वयंसेवक संघ के
+                        उत्तर प्रदेश के प्रांतीय संगठक (प्रान्त प्रचारक) बन गए।
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        पंडित दीनदयाल जी ने लखनऊ में राष्ट्र धर्म प्रकाशन नामक संस्थान की स्थापना की और यहां
+                        से “राष्ट्र धर्म” नामक मासिक पत्रिका का प्रकाशन आरम्भ किया।
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        सन 1950 में डॉक्टर श्यामा प्रसाद मुख़र्जी ने प्रधानमंत्री जवाहरलाल नेहरू के
+                        मंत्रिमंडल से त्यागपत्र दे दिया और देश में एक वैकल्पिक राजनीतिक मंच के सृजन के लिए
+                        श्री गोलवलकर जी से आदर्शवादी और देशभक्त नौजवानों को उपलब्ध कराने के लिए सहायता
+                        मांगी। इस राजनीतिक घटनाक्रम में दीनदयाल जी ने अत्यंत महत्त्वपूर्ण भूमिका निभायी।
+                        दिनांक 21 सितंबर, 1951 के ऐतिहासिक दिन उन्होंने उत्तर प्रदेश में एक राजनीतिक सम्मेलन
+                        का सफल आयोजन किया। इसी सम्मेलन में देश में एक नए राजनीतिक दल भारतीय जनसंघ की राज्य
+                        इकाई की स्थापना हुई। इसके एक माह के बाद 21 अक्टूबर,1951 को डॉक्टर श्यामा प्रसाद
+                        मुख़र्जी ने भारतीय जनसंघ के प्रथम अखिल भारतीय सम्मेलन की अध्यक्षता की।
+
+
+                    </p><br>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 boxx" data-aos="fade-up">
+                        <p>
+                            दीनदयाल जी में संगठन का अद्वितीय और अद्भुत कौशल था। समय बीतने के साथ
+                            भारतीय जनसंघ की विकास यात्रा में वह ऐतिहासिक दिन भी आया जब सन 1968 में
+                            विनम्रता की मूर्ति इस महान नेता को दल के अध्यक्ष के पद पर प्रतिष्ठित
+                            किया गया। अपने उत्तरदायित्व के निर्वहन और जनसंघ के देशभक्ति का सन्देश
+                            लेकर दीनदयाल जी ने दक्षिण भारत का भ्रमण किया।
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 boxx" data-aos="fade-up">
+                        <p>
+                            11 फरवरी, 1968 का दिन देश के राजनीतिक इतिहास में एक बेहद दु:खद और काला दिन
+                            है। इसी दिन अचानक पंडित दीनदयाल जी की आकस्मिक मृत्यु हुई। वे मुगलसराय रेलवे
+                            स्टेशन (वर्तमान में पंडित दीनदयाल उपाध्याय रेलवे स्टेशन) के निकट चलती
+                            रेलगाड़ी में संदिग्ध परिस्थितियों में उनकी मौत हो गयी। पंडित दीनदयाल जी के
+                            चाहने वाले और अनुयायी आज भी उस दुर्घटना से आहत हैं।
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!-- End About Us Section -->
+
+
+    </main><!-- End #main -->
+
+    <?php include('footer.php') ?>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
