a3a0758a59173f4ab2e0da14fc30cca0487454ab
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:53:53 +0530

    antyodya files

---
+++ antyodya.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>अंत्योदय - भारतीय जनता पार्टी, बिजनौर</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/" rel="icon"> -->
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+  <style>
+    .navheading {
+      display: inline-block;
+    }
+
+    .navheading h4 {
+      font-size: 30px;
+      margin-top: 12px;
+      text-align: center;
+      font-weight: 600;
+    }
+
+    .content .icon-box {
+      padding: 20px;
+      background-color: #f3f1f0;
+      border-radius: 8px;
+      text-align: justify;
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <?php include('header.php') ?>
+
+  <div class="">
+    <img src="assets/img/antodya/antodya.png" width="4000" class="img-fluid" alt="">
+  </div>
+
+  <div class="" style="height: 60px;background-color:green; text-align:center; color:orange;">
+    <div class="navheading" data-aos="fade-down">
+      <h4>अंत्योदय</h4>
+    </div>
+  </div>
+
+  <!-- ======= About Us Section ======= -->
+  <section id="" style="margin-left: 0%;">
+    <div class="container content">
+      <div class="row mb-3">
+        <div class="col-md-12 icon-box" data-aos="fade-up">
+          <p>
+            एकांत मानववाद और अंत्योदय का दर्शन पार्टी के मार्गदर्शक सिद्धांतों में से एक है। इस सिद्धांत को हम
+            सबका साथ सबका विकास के साथ मिला हुआ देख सकते हैं जो गरीब, ग्रामीण क्षेत्रों के विकास के लिए सरकार
+            द्वारा तय की गई नीतियों में भी नजर आता है।
+          </p>
+        </div>
+      </div>
+      <div class="row mb-3">
+        <div class="col-md-12 icon-box" data-aos="fade-up" data-aos-delay="100">
+          <p class="" style="text-align: justify;">
+            दीनदयाल जी और उनकी आर्थिक नीतियों ने हमेशा गरीबों की भलाई पर जोर देने की बात की है। उनके आर्थिक विचार
+            में पंक्ति के अंतिम पड़ाव पर खड़ा व्यक्ति शामिल रहा है। उन्होंने कहा था, ‘आर्थिक नीति निर्धारण और
+            प्रगति की सफलता का पैमाना यह नहीं है कि समाज के सबसे शीर्ष पर मौजूद व्यक्ति को उससे कितना फायदा मिल
+            रहा है बल्कि यह है कि समाज पर जो लोग सबसे नीचे हैं उन्हें उन नीतियों का कितना फायदा मिला है।’ अंत्योदय
+            का मतलब समाज के सबसे निचले स्तर पर मौजूद व्यक्ति का कल्याण है। उन्होंने यह भी कहा था, ‘यह हमारी सोच और
+            हमारे सिद्धांत हैं कि ये गरीब और अशिक्षित लोग हमारे ईश्वर हैं, यही हमारा सामाजिक और मानवीय धर्म है।‘
+          </p>
+        </div>
+      </div>
+      <div class="row mb-3">
+        <div class="col-md-12 icon-box" data-aos="fade-up" data-aos-delay="100">
+          <p class="" style="text-align: justify;">
+            उनके इसी अंत्योदय के विचार से प्रेरित होकर केन्द्र में प्रधानमंत्री नरेन्द्र मोदी के नेतृत्व में मौजूद
+            एनडीए सरकार और तमाम प्रदेशों में शासन करने वाली भाजपा सरकारें अंत्योदय के रास्ते पर बढ़ने की ओर अग्रसर
+            हैं तथा गरीब, ग्रामीण एवं किसानों के लिए और समाज के सबसे शोषित वर्ग से आने वाले युवाओं और महिलाओं के
+            कल्याण की ओर प्रतिबद्ध हैं।
+          </p>
+        </div>
+      </div>
+
+      <div class="row mb-3">
+        <div class="col-md-12 icon-box" data-aos="fade-up" data-aos-delay="200">
+          <p class="" style="text-align: justify;">
+            गरीब को सामाजिक सुरक्षा प्रदान करते हुए औसत जीवन स्तर में बढ़ोतरी हुई है। मुद्रा, जनधन, उज्जवला
+            (गरीबों को गैस कनेक्शन प्रदान करना) स्वच्छता मिशन, शौचालयों का निर्माण, दीनदयाल ग्राम ज्योति योजना,
+            आवास योजना, सस्ती दवाएं और इलाज, इन सभी योजनाओं पर कार्य को आगे बढ़ा दिया गया है।
+          </p>
+        </div>
+      </div>
+
+      <div class="row mb-3">
+        <div class="col-md-12 icon-box" data-aos="fade-up" data-aos-delay="300">
+          <p class="" style="text-align: justify;">
+            तकनीक के प्रयोग से कृषि में सुधार किया जा रहा है एवं किसानों की आय दुगनी करने के इरादे से सिंचाई
+            तकनीकों के लिए विशेष व्यवस्थाएं की जा रही हैं। केन्द्रीय बजट की सहायता से गांवों में भी कई निवेश किए
+            जा रहे हैं। दीनदयाल जी के विचारों से प्रेरित बीजेपी सरकार देश के संसाधनों का उपयोग केवल देश की उन्नति
+            के लिए करने के लिए प्रतिबद्ध है जिसके लिए यह सरकार भ्रष्टाचार पर लगाम लगाने, काले धन को रोकने और जनता
+            की कमाई की लूट रोकने के लिए कड़े कदम उठा रही है। एक भारत-श्रेष्ठ भारत का रास्ता गरीबी दूर करके ही
+            मिलेगा।
+          </p>
+        </div>
+      </div>
+    </div>
+  </section><!-- End  Section -->
+
+  <!-- ======= Our Team Section ======= -->
+  <section id="team" class="team section-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <h4 class="text-center mt-2" data-aos="fade-down">नवीनतम</h4>
+          <div class="border p-3" style="text-align: justify; background-color: white;" data-aos="fade-up">
+            <p>
+              भारतीय जनता पार्टी प्रदेश की सभी 403 विधानसभाओं में प्रबुद्ध वर्ग सम्मेलन आयोजित करेगी
+              <br>
+              <span>
+                SEPTEMBER 3, 2021
+              </span>
+            </p>
+
+            <p>
+              अपना घर हो इस सपने को सच्चे अर्थों में धरातल पर उतारने का काम प्रधानमंत्री श्री नरेन्द्र मोदी जी के
+              नेतृत्व में केंद्र व प्रदेश की योगी सरकार कर रही है – प्रदेश अध्‍यक्ष जी
+              <br>
+              <span>
+                SEPTEMBER 2, 2021
+              </span>
+            </p>
+
+            <p>
+              जन्माष्टमी पर्व प्रदेश की समृद्धि एवं खुशहाली लाएं – भाजपा उत्‍तर प्रदेश
+              <br>
+              <span>
+                AUGUST 30, 2021
+              </span>
+            </p>
+
+            <p>
+              जन्माष्टमी पर्व प्रदेश की समृद्धि एवं खुशहाली लाएं – भाजपा उत्‍तर प्रदेश
+              <br>
+              <span>
+                AUGUST 30, 2021
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-lg-4 footer-links text-left">
+          <h4 class="text-center mt-2" data-aos="fade-down">बीजेपी उत्तर प्रदेश फेसबुक</h4>
+          <aside class="widget" data-aos="fade-up">
+            <div class="fb-page" data-href="https://www.facebook.com/BJP4UP/" data-tabs="timeline" data-height="435" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+              <div cite="https://www.facebook.com/BJP4UP/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/BJP4UP/">Facebook</a></div>
+            </div>
+            <div class="textwidget custom-html-widget">
+              <script async src="https://connect.facebook.net/hi_IN/sdk.js#xfbml=1&version=v11.0" nonce="gMaj3cox"></script>
+            </div>
+          </aside>
+        </div>
+
+        <div class="col-lg-4">
+          <h4 class="text-center mt-2" data-aos="fade-down">बीजेपी उत्तर प्रदेश ट्विटर</h4>
+          <aside id="custom_html-3" class="widget_text widget widget_custom_html" data-aos="fade-up">
+            <div class="textwidget custom-html-widget"><a class="twitter-timeline" data-height="435" href="https://twitter.com/bjp4up?lang=en">Tweets by BJP4Bjinor</a>
+              <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+          </aside>
+        </div>
+
+      </div>
+  </section><!-- End Our Team Section -->
+
+  </main><!-- End #main -->
+
+  <?php include('footer.php') ?>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
