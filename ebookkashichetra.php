4c35527d00437e72c260acdc32d688dda7cdc348
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:58:49 +0530

    files

---
+++ ebookkashichetra.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>E-बुक : काशी क्षेत्र - भारतीय जनता पार्टी</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <main id="main">
+        <!-- ======= Section ======= -->
+        <section id="pricing" class="pricing">
+            <div class="container-fluid">
+
+                <div class="text-center mb-4" data-aos="fade-down" style="background-color: #f3f1f0; height: 40px;">
+                    <div class="kamaljyotiheading">
+                        <h2>
+                            E-बुक : काशी क्षेत्र
+                        </h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    अमेठी
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/AMETHI_FINAL-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    भदोही
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/BHADOHI_FINAL-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    चन्‍दौली
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/CHANDAULI_FINAL-page-001-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    गाजीपुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/GAZIPUR-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    जौनपुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/JAUNPUR_FINAL-1-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    वाराणसी जिला
+
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/Jila-Varanasi_FINAL-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मछलीशहर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/MACHHLI-SAHAR-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मीरजापुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/MIRZAPUR-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    प्रतापगढ़
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/PRATAPGARH_FINAL-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    प्रयागराज महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/PRAYAGRAJ-MAHANAGAR_FINAL-page-001-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    प्रयागराज गंगापार
+
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/pryagraj-gangapar_FINAL-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    सोनभद्र
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/SONBHADRA_FINAL-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    सुल्‍तानपुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/SULTANPUR_FINAL-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    प्रयागराज यमुनापार
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/YAMUNAPAR-page-001-208x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    कौशाम्‍बी
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/KAUSHAMBI_FINAL_Page_01-211x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    वाराणसी महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkashichetra/VARANASI-MAHANAGAR_FINAL_Page_01-212x300.webp" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- End Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include('footer.php') ?>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
