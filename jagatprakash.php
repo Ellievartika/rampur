cc83a44e1df1313cf43bcff25cac9e847da3cb05
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:04:15 +0530

    files

---
+++ jagatprakash.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>श्री जगत प्रकाश नड्डा - भारतीय जनता पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <!-- <link href="assets/img/" rel="icon"> -->
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="assets/css/navheading-partykebareme.css">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <div class="">
+        <img src="assets/img/headerimages/jagatnadda.webp" class="img-fluid" alt="">
+    </div>
+
+    <!-- navbar for heading -->
+    <div class="" style="height: 60px; background-color:green; text-align:center; color:orange;">
+        <div class="navheading">
+            <h4 data-aos="fade-down">श्री जगत प्रकाश नड्डा</h4>
+        </div>
+    </div>
+
+    <!-- ======= Section ======= -->
+    <section style="margin-left: 0%;" class="content">
+        <div class="container">
+            <div class="row">
+                <h4 data-aos="fade-down">श्री जगत प्रकाश नड्डा के बारे में</h4>
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        मूल रूप से हिमाचल प्रदेश के रहने वाले श्री जेपी नड्डा जी का जन्म बिहार की राजधानी
+                        पटना में 2 दिसंबर 1960 को हुआ था। पटना विश्वविद्यालय से बीए करने के बाद हिमाचल
+                        प्रदेश विश्वविद्यालय से एलएलबी की पढ़ाई की। युवावस्था में जय प्रकाश नारायण द्वारा
+                        चलाए गए विभिन्न आदोलनों में सक्रियता से प्रतिभाग किया। नड्डा जी अखिल भारतीय
+                        विद्यार्थी परिषद से जुड़े रहे और 1989 में एबीवीपी के राष्ट्रीय मंत्री चुने गए। वर्ष
+                        1991 में भारतीय जनता युवा मोर्चा के अध्यक्ष बनाए गए। वर्ष 1993 में पहली बार हिमाचल
+                        प्रदेश विधान सभा में विधायक बने और नेता प्रतिपक्ष चुने गए। वर्ष 1998 में दोबारा
+                        चुनाव जीते और भाजपा सरकार में स्वास्थ्य मंत्री बने। 2010 में भारतीय जनता पार्टी के
+                        राष्ट्रीय महामंत्री बनाए गए। 2012 में राज्यसभा सदस्य के रूप में चुने गए। 2014-2019
+                        तक भारत सरकार में स्वास्थ्य एवं परिवार कल्याण मंत्रालय की कमान संभाली। जून 2019 में
+                        नड्डा जी को भारतीय जनता पार्टी का राष्ट्रीय कार्यकारी अध्यक्ष और जनवरी 2020 में
+                        भारतीय जनता पार्टी का राष्ट्रीय अध्यक्ष चुना गया।
+                    </p>
+                </div>
+                <div id="btnheader" class="text-center">
+                    <div class="btnn">
+                        <a type="submit" href="https://jagatprakashnadda.in/" class="btn btn-lg">और पढ़े </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!-- End  Section -->
+
+    <?php include('footer.php') ?>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
