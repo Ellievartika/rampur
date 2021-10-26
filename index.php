72d15aaeceebee6072de50b1fd0ae4145fcd51f5
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:03:45 +0530

    index files

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>भारतीय जनता पार्टी, रामपुर</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/" rel="icon"> -->
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+</head>
+
+<body>
+  <?php include('header.php') ?>
+
+  <!-- ======= Carousel  ======= -->
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-aos="fade-down">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <!-- <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button> -->
+      <!-- <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button> -->
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="5000">
+        <img src="assets/img/slide/modiji.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="5000">
+        <img src="assets/img/slide/amitshah.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="5000">
+        <img src="assets/img/slide/yogiji.jpg" class="d-block w-100" alt="...">
+      </div>
+      <!-- <div class="carousel-item" data-bs-interval="5000">
+        <img src="assets/img/slide/subhashbal.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item" data-bs-interval="5000">
+        <img src="assets/img/slide/dindyan.jpg" class="d-block w-100" alt="...">
+      </div> -->
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <i class="fas fa-angle-double-left" style="font-size: 50px;"></i>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <i class="fas fa-angle-double-right" style="font-size: 50px;"></i>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+  <!-- carousel end -->
+
+  <!-- button section -->
+  <section class="my-2" id="btnheader">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-lg-4 my-2" data-aos="fade-up">
+          <div class="btnn">
+            <a type="submit" href="https://www.bjp.org/hi/jointheparty" class="btn btn-lg">ऑनलाइन सदस्यता</a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 my-2" data-aos="fade-up">
+          <div class="btnn">
+            <a type="submit" href="https://www.bjp.org/hi/donation" class="btn btn-lg">सहयोग करें</a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 my-2" data-aos="fade-up">
+          <div class="btnn">
+            <a type="submit" href="sevahisangathan.php" class="btn btn-lg">सेवा ही संगठन : E-बुक</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- button section end -->
+
+  <!-- ======= Services Section ======= -->
+  <div class="container services mt-4" id="services">
+    <div class="row">
+      <div class="col-lg-4 my-4">
+        <div class="icon-box">
+          <div class="icon" data-aos="fade-up"><img src="assets/img/indeximagesblog/orange-lotus-bjp-logo-vector-260nw-1750364873.jpg" data-aos="fade-up" alt=""></div>
+          <h4 class="title" data-aos="fade-down"><a href="">‘प्रधानमंत्री उज्ज्वला योजना’ से आज गरीब परिवार को निःशुल्क गैस सिलेण्डर
+              उपलब्ध होना, विपक्षी राजनैतिक दल को रास नहीं आ रहा – प्रदेश अध्‍यक्ष जी</a></h4>
+          <p class="description">लखनऊ 19 अगस्त 2021, ‘जन आशीर्वाद यात्रा‘ के तहत आज …</p>
+        </div>
+      </div>
+      <div class="col-lg-4 my-4">
+        <div class="icon-box">
+          <div class="icon" data-aos="fade-up"><img src="assets/img/indeximagesblog/orange-lotus-bjp-logo-vector-260nw-1750364873.jpg" data-aos="fade-up" alt=""></div>
+          <h4 class="title" data-aos="fade-down"><a href="">ब्रिटेन के भारत में उच्चायुक्त श्री एलेक्स एलिस ने राज्य मुख्यालय में प्रदेश
+              अध्यक्ष श्री स्वतंत्र देव सिंह से भेंट कर विभिन्न विषयों पर चर्चा की</a></h4>
+          <p class="description">लखनऊ 20 अगस्त 2021, ब्रिटेन के भारत में उच्चायुक्त श्री …</p>
+        </div>
+      </div>
+      <div class="col-lg-4 my-4">
+        <div class="icon-box">
+          <div class="icon" data-aos="fade-up"><img src="assets/img/jila/abhay-gupta-rampur.webp" data-aos="fade-up" alt=""></div>
+          <h4 class="title mt-3" data-aos="fade-down"><a href="">भाजपा का हर एक कार्यकर्ता राष्ट्रीय विचारधारा और सांस्कृतिक राष्ट्र के
+              निर्माण के लिए प्रतिज्ञा बद्ध है – जिला अध्‍यक्ष जी</a></h4>
+          <p class="description">लखनऊ 19 अगस्त 2021, भारतीय जनता पार्टी का हर एक …</p>
+        </div>
+      </div>
+      <div class="col-lg-4 my-4">
+        <div class="icon-box">
+          <div class="icon" data-aos="fade-up"><img src="assets/img/indeximagesblog/uppresident.jpg" data-aos="fade-up" alt=""></div>
+          <h4 class="title mt-3" data-aos="fade-down"><a href="">भाजपा सरकार के द्वारा गांव, गरीब, किसान की खुशहाली व
+              प्रगति के लिए किये गए कार्यों से भाजपा पर किसानों का विश्वास और भरोसा मजबूत हुआ है</a>
+          </h4>
+          <p class="description">लखनऊ 12 अगस्त 2021, भारतीय जनता पार्टी के प्रदेश अध्यक्ष …</p>
+        </div>
+      </div>
+      <div class="col-lg-4 my-4">
+        <div class="icon-box">
+          <div class="icon" data-aos="fade-up"><img src="assets/img/indeximagesblog/ramnath.jpg" data-aos="fade-up" alt=""></div>
+          <h4 class="title mt-3" data-aos="fade-down"><a href="">केन्द्रीय मंत्रिमण्डल में उत्तर प्रदेश से सम्मिलित हुए नए मंत्रियों के
+              द्वारा
+              जनता का आशीर्वाद लेने के लिए शुरू की गई ‘जन आशीर्वाद यात्रा‘ आज भी जारी रही</a></h4>
+          <p class="description">लखनऊ 18 अगस्त 2021, माननीय प्रधानमंत्री श्री नरेन्द्र मोदी जी …</p>
+        </div>
+      </div>
+      <div class="col-lg-4 my-4">
+        <div class="icon-box">
+          <div class="icon" data-aos="fade-up"><img src="assets/img/indeximagesblog/yogi.jpg" data-aos="fade-up" alt=""></div>
+          <h4 class="title mt-3" data-aos="fade-down"><a href="">केन्द्रीय मंत्रिमण्डल में उत्तर प्रदेश से सम्मिलित हुए नए मंत्रियों के
+              द्वारा लगातार उत्तर प्रदेश में जन आशीर्वाद यात्रा चल रही है</a></h4>
+          <p class="description">लखनऊ 19 अगस्त 2021, ‘जन आशीर्वाद यात्रा‘ के तहत आज …</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= yojnaye ======= -->
+  <div class="container">
+    <div class="row" data-aos="fade-down">
+      <div class="col-lg-12 yojyanayetitle">
+        <h2 class="text-center" style="font-size: 30px; font-weight: 600;">योजनाएं</h2>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-6 my-3" data-aos="fade-up">
+        <img src="assets/img/portfolio/gramin.jpg" class="img-fluid" alt="img not found">
+      </div>
+
+      <div class="col-lg-6 my-3" data-aos="fade-up">
+        <img src="assets/img/portfolio/janarogya.jpg" class="img-fluid" alt="img not found">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-6 my-3" data-aos="fade-up">
+        <img src="assets/img/portfolio/shakti.jpg" class="img-fluid" alt="img not found">
+      </div>
+
+      <div class="col-lg-6 my-3" data-aos="fade-up">
+        <img src="assets/img/portfolio/ujjwala.jpg" class="img-fluid" alt="img not found">
+      </div>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
