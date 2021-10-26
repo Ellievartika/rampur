0c5f4b7d2b2b22920a2a382072cdade2a26b2929
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:07:21 +0530

    files

---
+++ narendra.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>श्री नरेन्द्र मोदी - भारतीय जनता पार्टी, बिजनौर</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <!-- <link href="assets/img/" rel="icon"> -->
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="assets/css/navheading-partykebareme.css">
+
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <div class="">
+        <img src="assets/img/headerimages/narendra-modi.jpg" class="img-fluid" alt="">
+    </div>
+
+    <!-- navbar for heading -->
+    <div class="" style="height: 60px; background-color:green; text-align:center; color:orange;">
+        <div class="navheading">
+            <h4 data-aos="fade-down">श्री नरेन्द्र मोदी</h4>
+        </div>
+    </div>
+
+    <!-- ======= About Us Section ======= -->
+    <section style="margin-left: 0%;" class="content">
+        <div class="container">
+            <div class="row">
+                <h4 data-aos="fade-down">श्री नरेंद्र मोदी के बारे में</h4>
+                <div class="col-md-12 boxx" data-aos="fade-up">
+                    <p>
+                        श्री नरेन्द्र मोदी ने 26 मई 2014 को भारत के प्रधानमंत्री पद की शपथ ली और वे भारत के
+                        प्रथम प्रधानमंत्री हैं जिनका जन्म आजादी के बाद हुआ है। ऊर्जावान, समर्पित एवं दृढ़
+                        निश्चय वाले नरेन्द्र मोदी एक अरब से अधिक भारतीयों की आकांक्षाओं और आशाओं के द्योतक
+                        हैं। मेहनती, लगनशील और जुझारू श्री नरेन्द्र मोदी करोड़ों भारतीयों की उम्मीदों का
+                        चेहरा हैं। जबसे उन्होंने प्रधानमंत्री पद संभाला है, तबसे देश को उस विकास की ओर ले
+                        जाने के लिए अग्रसर हैं जहां हर देशवासी अपनी आशाओं और आकांक्षाओं को पूरा कर सके।
+                        प्रधानमंत्री जी अंत्योदय के विचार से काफी प्रेरित हैं, वह अंतिम पायदान पर खड़े एक-एक
+                        व्यक्ति का पूर्ण विकास करना चाहते हैं। उनके अनोखे विचारों, निर्णयों ने उन्हें देश और
+                        विदेश में काफी लोकप्रिया बना दिया है। उन्होंने गर्वनेंस को काफी सुलभ बनाया है जिसका
+                        फायदा हर व्यक्ति को मिल रहा है।
+                    </p>
+                </div>
+                <div id="mt-3" class="text-center btnnarendra">
+                        <div class="btnn">
+                            <a type="submit" href="https://www.narendramodi.in/" class="btn btn-lg">और पढ़े </a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </section><!-- End Section -->
+
+    <?php include('footer.php') ?>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
