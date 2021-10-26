e0c5368f6f9070b23c1bf21840e3d0776aa251f6
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:00:36 +0530

    files

---
+++ ekatamak.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>एकात्म मानववाद - भारतीय जनता पार्टी, बिजनौर</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/" rel="icon"> -->
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+  <style>
+    .navheading {
+      display: inline-block;
+    }
+
+    .navheading h4 {
+      font-size: 30px;
+      margin-top: 12px;
+      text-align: center;
+      font-weight: 600;
+    }
+
+    .content ul li {
+      padding: 20px;
+      background-color: #f3f1f0;
+      border-radius: 8px;
+      text-align: justify;
+      margin-bottom: 20px;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include('header.php') ?>
+
+  <div class="">
+    <img src="assets/img/antodya/antodya.png" class="img-fluid" alt="">
+  </div>
+
+  <!-- navbar for heading -->
+  <div class="" style="height: 60px;background-color:green; text-align:center; color:orange;">
+    <div class="navheading">
+      <h4 data-aos="fade-down">एकात्म मानववाद</h4>
+    </div>
+  </div>
+  <!-- end heading navbar -->
+
+  <!-- ======= About Us Section ======= -->
+  <section class="content" style="margin-left: 0%;">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <ul>
+            <li data-aos="fade-up">
+              <p>
+                भाजपा संविधान की धारा तीन के अन्तर्गत ‘एकात्मक मानववाद’ भाजपा का मूल दर्शन है। पंडित दीनदयाल
+                उपाध्याय ने अपने सुदीर्घ चिन्तन, अध्ययन एवं मनन के बाद सन् 1964-65 में विचारधारा के नाते इसका प्रणयन
+                किया।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                पाश्चात्य राजनैतिक चिन्तन ने मानव को ‘सेक्यूलरवाद, व्यक्तिवाद (पूंजीवाद) समाजवाद एवं साम्यवाद की
+                विचार धाराएं दी थीं। स्वतंत्र भारत का नेतृत्व भी इन्हीं वादों में भारत का भविष्य खोज रहा था। दीनदयाल
+                जी ने इस खोज में हस्तक्षेप करते हुए यह सवाल खड़ा किया कि जब हमने पाश्चात्य साम्राज्यवाद को नकार दिया,
+                तब अब हमारी क्या मजबूरी है कि हम पाश्चात्य वादों का अनुगमन करें।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                सामान्यतः भारत के राजनैतिक क्षेत्र में स्थापित सभी दल यह सोचते थे कि हमें कुछ संशोधनों के साथ इन
+                पाश्चात्य वादों को ही स्वीकारना पडे़गा क्योंकि हमारे पास कोई अन्य चिंतन नहीं है। हम तो राष्ट्र थे ही
+                नहीं। पाश्चात्यों ने ही आकर हमको राष्ट्र बनने के लिए तैयार किया है। उनका विचार है हम राष्ट्र बनने जा
+                रहे हैं या हम नवोदित राष्ट्र है, आदि।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                भारतीय जनसंघ या भारतीय जनता पार्टी भारत को प्राचीन एवं सनातन राष्ट्र मानती है। पश्चिम की
+                राष्ट्र-राज्य परिकल्पना से पुरानी कल्पना भारत के ‘सांस्कृतिक राष्ट्रवाद’ की है। भारतीय संस्कृति की
+                एक गौरवसम्पन्न ज्ञान-परम्परा हैं हमें इसी ज्ञान-परम्परा में भारत का भविष्य खोजना चाहिए।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                मानव की तरफ देखने की पाश्चात्य दृष्टि खण्डित हैं उनका व्यक्तिवाद, समाजवाद का दुश्मन है तथा समाजवाद,
+                व्यक्तिवाद का शत्रु है। वे प्रकृति पर मानव की विजय चाहते हैं, इस प्रकार यहां भी प्रकृति बनाम मानव
+                उनका
+                समीकरण है। सेक्यूलरवाद को अपना कर उन्होंने अपने सार्वजनिक जीवन को अध्यात्म से काट लिया, अतः भौतिकवाद
+                बनाम अध्यात्म, स्टेट बनाम चर्च तथा रिलिजन बनाम सांइस के द्वंद्वमूलक समीकरण वहां उत्पन्न हुये।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                दीनदयाल जी मानते थे कि पश्चिम की यह बहस भी एक मानवीय बहस है, इसे हमें जानना चाहिए तथा इससे कुछ सीखना
+                भी चाहिये, लेकिन हमें इन द्वंद्व्रमूलक निष्कर्षों का अनुयायी नहीं बनना चाहिये।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                अतः मौलिक भारतीय चिन्तन के आधार पर विकल्प देने की जिम्मेदारी उन्होंने स्वयं स्वीकार की। भारतीय जनसंघ
+                की पहली पीढ़ी के सभी कार्यकर्ता इस काम में लगे। 1959 का पूना अभ्यासवर्ग, 1964 का ग्वालियर अभ्यास वर्ग
+                तथा 1964 के संघ शिक्षा वर्गों के इस दृष्टि से विशेष महत्व है। इन वर्गों में परिपक्व हुये विचारों को
+                दीनदयाल जी ने सिद्धांत और नीति प्रलेख में ‘एकात्म मानववाद’ नाम से प्रस्तुत किया। 1965 में भारतीय
+                जनसंघ के विजयवाड़ा वार्षिक अधिवेशन में इसे मूल दर्शन के रूप में स्वीकार किया गया तथा 1985 में भारतीय
+                जनता पार्टी ने भी इसे अपने मूल दर्शन के रूप में स्वीकार किया।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                यह विचार व्यक्ति बनाम समाज नहीं वरन व्यक्ति और समाज की एकात्मता का विचार है। यह मानव बनाम प्रकृति
+                नहीं वरन मानव के साथ प्रकृति की एकात्मता का विचार है। भौतिक बनाम अध्यात्मिक नहीं वरन इनकी एकात्मता
+                का विचार है। भारत में इसे धर्म कहा गया है ‘यतो अभ्युदय निःश्रेयस संसिद्धि स धर्म।’ अर्थात् यह
+                व्यष्टि, समिष्ट, सृष्टि व परमेष्ठी की एकात्मता का विचार है।
+              </p>
+            </li>
+            <li data-aos="fade-up">
+              <p>
+                भारतीय परम्परा इन पृथकताओं का निषेध करती है वह जड़-चेतन सभी से अपनी रिश्ते स्थापित करती है। धरती
+                ‘माता’ है चन्द्रमा मामा है पर्वत ’देवता’ है, नदियां ‘माता’ है। समाज का हर व्यक्ति परस्पर जुड़ा हुआ
+                है, यह संसार परायेपन की जगह नहीं, यह ‘वसुधा तो एक कुटुम्ब’ है आदि विचार मानव को असम्बद्धता, पृथकता
+                तथा द्वन्द्वशीलता के सम्बंधों से निजात दिलाते है।
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section><!-- End About Us Section -->
+
+  <!-- ======= Our Team Section ======= -->
+  <section id="team" class="team section-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <h4 class="text-center mt-2" data-aos="fade-down">नवीनतम</h4>
+          <div class="border" style="text-align: justify; background-color: white;" data-aos="fade-up">
+            <div class="mx-2 my-2">
+              <p>
+                भारतीय जनता पार्टी प्रदेश की सभी 403 विधानसभाओं में प्रबुद्ध वर्ग सम्मेलन आयोजित करेगी
+                <br>
+                <span>
+                  SEPTEMBER 3, 2021
+                </span>
+              </p>
+
+              <p>
+                अपना घर हो इस सपने को सच्चे अर्थों में धरातल पर उतारने का काम प्रधानमंत्री श्री नरेन्द्र मोदी जी के
+                नेतृत्व में केंद्र व प्रदेश की योगी सरकार कर रही है – प्रदेश अध्‍यक्ष जी
+                <br>
+                <span>
+                  SEPTEMBER 2, 2021
+                </span>
+              </p>
+
+              <p>
+                जन्माष्टमी पर्व प्रदेश की समृद्धि एवं खुशहाली लाएं – भाजपा उत्‍तर प्रदेश
+                <br>
+                <span>
+                  AUGUST 30, 2021
+                </span>
+              </p>
+
+              <p>
+                जन्माष्टमी पर्व प्रदेश की समृद्धि एवं खुशहाली लाएं – भाजपा उत्‍तर प्रदेश
+                <br>
+                <span>
+                  AUGUST 30, 2021
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 footer-links">
+          <h4 class="text-center mt-2" data-aos="fade-down">बीजेपी उत्तर प्रदेश फेसबुक</h4>
+          <aside class="widget" data-aos="fade-up">
+            <div class="fb-page" data-href="https://www.facebook.com/BJP4UP/" data-tabs="timeline" data-height="410" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+              <div cite="https://www.facebook.com/BJP4UP/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/BJP4UP/">Facebook</a></div>
+            </div>
+            <div class="textwidget custom-html-widget">
+              <script async src="https://connect.facebook.net/hi_IN/sdk.js#xfbml=1&version=v11.0" nonce="gMaj3cox"></script>
+            </div>
+          </aside>
+        </div>
+
+        <div class="col-lg-4">
+          <h4 class="text-center mt-2" data-aos="fade-down">बीजेपी उत्तर प्रदेश ट्विटर</h4>
+          <aside id="custom_html-3" class="widget_text widget widget_custom_html" data-aos="fade-up">
+            <div class="textwidget custom-html-widget"><a class="twitter-timeline" data-height="410" href="https://twitter.com/bjp4up?lang=en">Tweets by BJP4Bjinor</a>
+              <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+          </aside>
+        </div>
+
+      </div>
+    </div>
+  </section><!-- End Our Team Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include('footer.php') ?>
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
