9163410cb3d5f42612001ee252a9e008b81ffc00
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:58:18 +0530

    files

---
+++ ebookkanpurchetra.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>E-बुक : कानपुर क्षेत्र - भारतीय जनता पार्टी</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <main id="main">
+        <!-- ======= Section ======= -->
+        <section id="pricing" class="pricing">
+            <div class="container-fluid">
+
+                <div class="text-center mb-4" data-aos="fade-down" style="background-color: #f3f1f0; height: 40px;">
+                    <div class="kamaljyotiheading">
+                        <h2>
+                            E-बुक : कानपुर क्षेत्र
+                        </h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    औरेया
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Auraiya_Page_01-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बॉंदा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Banda_Page_01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    चित्रकूट
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Chitrakoot_Page_01-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    इटावा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Etawah_Page_01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    फर्रूखाबाद
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Farrukhabad_Page_01-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    फतेहपुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Fatehpur_Page_01-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    हमीरपुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Hamirpur_Page_01-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    जालौन
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Jaluan_Page_01-212x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    झांसी जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Jhansi-Jila_Page_01-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    झांसी महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Jhansi-Mahanagar_Page_01-212x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    कन्‍नौज
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Kannauj_Page_01-212x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    कानपुर देहात
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Kanpur-Dehat_Page_01-214x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    कानपुर ग्रामीण
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Kanpur-Gramin_Page_01-208x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    कानपुर महानगर(दक्षिण)
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Kanpur-Mahanagar-Dakshin_Page_01-208x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    कानपुर महानगर (उत्‍तर)
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Kanpur-Mahanagar-Uttar_Page_01-208x300.jpg"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    ललितपुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Lalitpur_Page_01-208x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    महोबा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookkanpurchetra/Mahoba_Page_01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section><!-- End Section -->
+
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include('footer.php') ?>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
