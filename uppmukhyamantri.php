cb9da4f4c1a294d816fd8509d57a0b506a72356d
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:11:03 +0530

    files

---
+++ uppmukhyamantri.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>भारतीय जनता पार्टी, उत्तर प्रदेश</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <!-- <link href="assets/img/" rel="icon"> -->
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <!-- ======= About Us Section ======= -->
+    <section id="img-cards" class="img-cards" style="margin-top: 10px;">
+        <div class="container">
+
+            <div class="text-center mb-4" data-aos="fade-down" style="background-color: #f3f1f0; height: 40px;">
+                <strong>
+                    <h2>
+                        उप मुख्यमंत्री
+                    </h2>
+                </strong>
+            </div>
+
+            <div class="row text-center justify-content-center">
+                <div class="col-lg-4 mb-4" data-aos="fade-up">
+                    <div class="content">
+                        <div class="card">
+                            <img class="card-img-top img-fluid" src="assets/img/uppmukhyamantri/bjpblacklogo1.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: bold;"> श्री केशव प्रसाद मौर्या </h5>
+                                <hr>
+                                <p class="" style="font-size: 18px; font-weight: 600;">
+                                    उप मुख्यमंत्री
+                                </p>
+                                <p class="card-text">
+                                    विभाग - लोक निर्माण, खाद्य प्रसंस्करण, मनोरंजन कर, सार्वजनिक उद्यम
+                                </p>
+                                <div class="text-center" data-aos="fade-up">
+                                    <a href=""><i class="fab fa-facebook-square mx-2"></i></a>
+                                    <a href=""><i class="fab fa-twitter-square mx-2"></i></a>
+                                </div>
+                                <div class="text-center links my-4" data-aos="fade-down">
+                                    <a href="http://keshavprasadmaurya.com/">http://keshavprasadmaurya.com/</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-4 mb-4" data-aos="fade-up">
+                    <div class="content">
+                        <div class="card">
+                            <img class="card-img-top img-fluid" src="assets/img/uppmukhyamantri/dinesh-sharma-1.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title" style="font-weight: bold;">डॉ० दिनेश शर्मा</h5>
+                                <hr>
+                                <p class="" style="font-size: 18px; font-weight: 600;">
+                                    उप मुख्यमंत्री
+                                </p>
+                                <p class="card-text">
+                                    विभाग - माध्यमिक शिक्षा, उच्च शिक्षा, विज्ञान एवं प्रौद्योगिकी, इलेक्ट्रानिक्स, सूचना प्रौद्योगिकी
+                                </p>
+                                <div class="text-center" data-aos="fade-up">
+                                    <a href="https://www.facebook.com/profdineshsharmabjp/"><i class="fab fa-facebook-square mx-2"></i></a>
+                                    <a href="https://twitter.com/drdineshbjp"><i class="fab fa-twitter-square mx-2"></i></a>
+                                </div>
+                                <div class="text-center links my-4" data-aos="fade-down">
+                                    <a href="http://www.drdineshsharma.in/hi">http://www.drdineshsharma.in/hi</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include('footer.php') ?>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
