676e94639476e60dd59ccd9b813ece738b8ae7e6
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:09:24 +0530

    files

---
+++ shyamaprasad.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>डॉ. श्यामा प्रसाद मुखर्जी - भारतीय जनता पार्टी, बिजनौर</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/" rel="icon"> -->
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="assets/css/all.css"> -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+  <link rel="stylesheet" href="assets/css/navheading-partykebareme.css">
+</head>
+
+<body>
+
+  <?php include('header.php') ?>
+
+  <div class="headerimage">
+    <img src="assets/img/headerimages/website-cover-shyama-prasad-mukharji-1.webp" class="img-fluid" alt="">
+  </div>
+
+  <!-- navbar for heading -->
+  <div class="" style="height: 60px; background-color:green; text-align:center; color:orange;">
+    <div class="navheading">
+      <h4 data-aos="fade-down"> संक्षिप्त जीवन परिचय</h4>
+    </div>
+  </div>
+  <!-- end heading navbar -->
+
+  <!-- ======= About Us Section ======= -->
+  <section style="margin-left: 0%;" class="content">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 boxx" data-aos="fade-up">
+          <p>
+            डॉ. श्यामा प्रसाद मुखर्जी जम्मू-कश्मीर को भारत का पूर्ण और अभिन्न अंग बनाना चाहते थे। संसद में अपने
+            भाषण में उन्होंनें धारा-370 को समाप्त करने की भी जोरदार वकालत की। अगस्त 1952 में जम्मू कश्मीर की विशाल
+            रैली में उन्होंने अपना संकल्प व्यक्त किया था कि ”या तो मैं आपको भारतीय संविधान प्राप्त कराऊंगा या फिर
+            इस उद्देश्य की पूर्ति के लिये अपना जीवन बलिदान कर दूंगा”। डॉ. मुखर्जी अपने संकल्प को पूरा करने के लिये
+            1953 में बिना परमिट लिये जम्मू-कश्मीर की यात्रा पर निकल पड़े। वहां पहुंचते ही उन्हें गिरफ्तार कर लिया
+            गया। 23 जून 1953 को जेल में रहस्यमय परिस्थितियों में उनकी मृत्यु हो गयी। जेल में उनकी मृत्यु ने देश को
+            हिलाकर रख दिया और परमिट सिस्टम समाप्त हो गया। उन्होंने कश्मीर को लेकर एक नारा दिया था, ”नहीं चलेगा एक
+            देश में दो विधान, दो प्रधान और दो निशान”।
+          </p>
+          <p>
+          अपने पुत्र की असामयिक मृत्यु का समाचार सुनने के पश्चात डॉ. मुखर्जी की माता योगमाया देबी ने कहा था :
+          </p>
+          <p>
+          ”मेरे पुत्र की मृत्यु भारत माता के पुत्र की मृत्यु है”
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 boxx" data-aos="fade-up" data-aos-delay="200">
+          <p>
+            भारत माता के इस वीर पुत्र का जन्म 6 जुलाई 1901 को एक प्रतिष्ठित परिवार में हुआ था। इनके पिता सर आशुतोष
+            मुखर्जी बंगाल में एक शिक्षाविद् और बुद्धिजीवी के रूप में प्रसिद्ध थे। कलकत्ता विश्वविद्यालय से स्नातक
+            होने के पश्चात श्री मुखर्जी 1923 में सेनेट के सदस्य बने। अपने पिता की मृत्यु के पश्चात, 1924 में
+            उन्होंने कलकत्ता उच्च न्यायालय में अधिवक्ता के रूप में नामांकन कराया। 1926 में उन्होंने इंग्लैंड के
+            लिए प्रस्थान किया जहां लिंकन्स इन से 1927 में बैरिस्टर की परीक्षा उत्तीर्ण की। 33 वर्ष की आयु में
+            कलकत्ता विश्वविद्यालय के कुलपति नियुक्त हुए और विश्व का सबसे युवा कुलपति होने का सम्मान प्राप्त किया।
+            श्री मुखर्जी 1938 तक इस पद को सुशोभित करते रहे। अपने कार्यकाल में उन्होंने अनेक रचनात्मक सुधार किये
+            तथा इस दौरान ‘कोर्ट एंड काउंसिल ऑफ इंडियन इंस्टीट्यूट ऑफ साइंस बैंगलोर’ तथा इंटर यूनिवर्सिटी बोर्ड के
+            सक्रिय सदस्य भी रहे।
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 boxx" data-aos="fade-up" data-aos-delay="300">
+          <p>
+            कांग्रेस प्रत्याशी और कलकत्ता विश्वविद्यालय के प्रतिनिधि के रूप में उन्हें बंगाल विधान परिषद का सदस्य
+            चुना गया किन्तु कांग्रेस द्वारा विधायिका के बहिष्कार का निर्णय लेने के पश्चात उन्होंने त्यागपत्र दे
+            दिया। बाद में डॉ. मुखर्जी स्वतंत्र प्रत्याशी के रूप में चुनाव लड़े और निर्वाचित हुए।
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 boxx" data-aos="fade-up" data-aos-delay="300">
+          <p>
+            पंडित जवाहरलाल
+            नेहरू ने उन्हें अंतरिम सरकार में उद्योग एवं आपूर्ति मंत्री के रूप में शामिल किया। नेहरू और पाकिस्तान
+            के प्रधानमंत्री लियाकत अली के बीच हुए समझौते के पश्चात 6 अप्रैल 1950 को उन्होंने मंत्रिमंडल से
+            त्यागपत्र दे दिया। राष्ट्रीय स्वयंसेवक संघ के सर-संघचालक गुरु गोलवलकर जी से परामर्श लेकर श्री मुखर्जी
+            ने 21 अक्टूबर 1951 को राष्ट्रीय जनसंघ की स्थापना की। 1951-52 के आम चुनावों में राष्ट्रीय जनसंघ के तीन
+            सांसद चुने गए जिनमें एक डॉ. मुखर्जी भी थे। तत्पश्चात उन्होंने संसद के अन्दर 32 लोकसभा और 10 राज्यसभा
+            सांसदों के सहयोग से नेशनल डेमोक्रेटिक पार्टी का गठन किया। डॉ. मुखर्जी भारत की अखंडता और कश्मीर के विलय
+            के दृढ़ समर्थक थे। उन्होंने अनुच्छेद 370 के प्रावधानों को भारत के बाल्कनीकरण की संज्ञा दी थी। अनुच्छेद
+            370 के राष्ट्रघातक प्रावधानों को हटाने के लिए भारतीय जनसंघ ने हिन्दू महासभा और रामराज्य परिषद के साथ
+            सत्याग्रह आरंभ किया। डॉ. मुखर्जी 11 मई 1953 को कुख्यात परमिट सिस्टम का उलंघन करके कश्मीर में प्रवेश
+            करते हुए गिरफ्तार कर लिए गए। गिरफ्तारी के दौरान ही विषम परिस्थितियों में 23 जून, 1953 को उनका
+            स्वर्गवास हो गया।
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 boxx" data-aos="fade-up" data-aos-delay="300">
+          <p>
+            एक दक्ष राजनीतिज्ञ, विद्वान और स्पष्टवादी के रूप में वे अपने मित्रों और शत्रुओं द्वारा सामान रूप से
+            सम्मानित थे। एक महान देशभक्त और संसद शिष्ट के रूप में भारत उन्हें सम्मान के साथ याद करता है।
+          </p>
+        </div>
+      </div>
+    </div>
+  </section><!-- End About Us Section -->
+
+
+  </main><!-- End #main -->
+
+  <?php include('footer.php') ?>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
