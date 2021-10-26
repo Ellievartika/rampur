eeacb3307623e36f66e9d9427c217535a84c6afd
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:55:11 +0530

    files

---
+++ chiefminister.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>मुख्यमंत्री - भारतीय जनता पार्टी, बिजनौर</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<?php include('header.php') ?>
+
+  <main id="main">
+    <!-- ======= Section ======= -->
+    <section id="pricing" class="pricing">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="box" data-aos="fade-down">
+              <img src="assets/img/yogiimg/Yogi-Adityanath-1.jpg" class="img-fluid" alt="">
+
+              <div class="btn-wrap" data-aos="fade-down">
+                <a href="https://www.facebook.com/MYogiAdityanath/" class="btn-icon"><i class="fab fa-facebook-square mx-2"></i></a>
+                <a href="https://twitter.com/myogiadityanath" class="btn-icon"><i class="fab fa-twitter-square mx-2"></i></a>
+                <a href="https://www.instagram.com/accounts/login/" class="btn-icon"><i class="fab fa-instagram mx-2"></i></a>
+              </div>
+
+              <div class="btn-wrap" data-aos="fade-down">
+                <a href="http://yogiadityanath.in/" class="btn-buy">http://www.yogiadityanath.in</a>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-lg-8">
+            <div class="box" data-aos="fade-up">
+              <h1 data-aos="fade-up">मा० योगी आदित्य नाथ</h1>
+              <p style="text-align: justify;" data-aos="fade-up">
+                योगीजी का जन्म देवाधिदेव भगवान् महादेव की उपत्यका में स्थित देव-भूमि उत्तराखण्ड में 5 जून सन् 1972 को
+                हुआ। शिव अंश की उपस्थिति ने छात्ररूपी योगी जी को शिक्षा के साथ-साथ सनातन हिन्दू धर्म की विकृतियों एवं उस
+                पर हो रहे प्रहार से व्यथित कर दिया। प्रारब्ध की प्राप्ति से प्रेरित होकर आपने 22 वर्ष की अवस्था में
+                सांसारिक जीवन त्यागकर संन्यास ग्रहण कर लिया। आपने विज्ञान वर्ग से स्नातक तक शिक्षा ग्रहण की तथा छात्र
+                जीवन में विभिन्न राष्ट्रवादी आन्दोलनों से जुड़े रहे।
+              </p>
+
+
+              <div class="btn-wrap" data-aos="fade-up">
+                <a href="http://www.yogiadityanath.in/About.aspx" class="btn-buy">अधिक जाने</a>
+              </div>
+
+              <ul style="text-align: left;" data-aos="fade-up">
+                <li>
+                  <a href="">जन्म की तारीख - 05 जून 1972</a>
+                </li>
+                <br>
+                <li>
+                  <a href="">शैक्षिक योग्यता - बी० एस० सी० ( मैथ ),</a>
+                </li>
+                <li>
+                  <a href="">स्थायी पता - श्री गोरखनाथ मन्दिर, गोरखपुर (उत्तर प्रदेश) 273015 - फोन - (0551) 2255453,
+                    2255454</a>
+                </li>
+                <li>
+                  <a href="">वर्तमान पता- 5, कालीदास मार्ग,मुख्यमंत्री आवास, उत्तर प्रदेश - फोन - 0522 - 2236838,
+                    2235599, 2236985</a>
+                </li>
+              </ul>
+
+              <p style="text-align: justify;" data-aos="fade-down">
+                विभाग – गृह, आवास एवं शहरी नियोजन, राजस्व, खाद्य एवं रसद, नागरिक आपूर्ति, खाद्य सुरक्षा एवं औषधि
+                प्रशासन, अर्थ एवं संख्या, भूतत्व एवं खनिजकर्म, बाढ़ नियंत्रण, कर निबंधन, कारागार, सामान्य प्रशासन,
+                सचिवालय प्रशासन, गोपन, सर्तकता, नियुक्ति, कार्मिक, सूचना, निर्वाचन, संस्थागत वित्त, नियोजन, राज्य
+                सम्पत्ति, नगर भूमि, उत्तर प्रदेश पुनर्गठन समन्वय, प्रशासनिक सुधार, कार्यक्रम कार्यान्वयन, राष्ट्रीय
+                एकीकरण, अवस्थापना, भाषा, वाह्य सहायतित परियोजना, अभाव, सहायता एवं पुनर्वास, लोक सेवा प्रबंधन, किराया
+                नियंत्रण, उपभोक्ता संरक्षण, बांट माप विभाग
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Section -->
+
+    <!-- yogi years -->
+    <section id="pricing" class="pricing">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 mb-4">
+            <div class="box" data-aos="fade-up">
+              <img src="assets/img/yogiimg/yogismall.jpg" style="max-width: 140px;" class="img-fluid" alt="">
+              <span>
+                <h4>1998-99</h4>
+              </span>
+              <span>
+                <h5>सांसद- गोरखपुर </h5>
+              </span>
+            </div>
+          </div>
+
+          <div class="col-lg-4  mb-4">
+            <div class="box" data-aos="fade-up">
+              <img src="assets/img/yogiimg/yogismall.jpg" style="max-width: 140px;" class="img-fluid" alt="">
+              <span>
+                <h4>1999-2004</h4>
+              </span>
+              <span>
+                <h5>सांसद- गोरखपुर </h5>
+              </span>
+            </div>
+          </div>
+
+          <div class="col-lg-4  mb-4">
+            <div class="box" data-aos="fade-up">
+              <img src="assets/img/yogiimg/yogismall.jpg" style="max-width: 140px;" class="img-fluid" alt="">
+              <span>
+                <h4>2004-2009</h4>
+              </span>
+              <span>
+                <h5>सांसद- गोरखपुर </h5>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 mb-4">
+            <div class="box" data-aos="fade-up">
+              <img src="assets/img/yogiimg/yogismall.jpg" style="max-width: 140px;" class="img-fluid" alt="">
+              <span>
+                <h4>2009-2014</h4>
+              </span>
+              <span>
+                <h5>सांसद- गोरखपुर </h5>
+              </span>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class="box" data-aos="fade-up">
+              <img src="assets/img/yogiimg/yogismall.jpg" style="max-width: 140px;" class="img-fluid" alt="">
+              <span>
+                <h4>2014-2017</h4>
+              </span>
+              <span>
+                <h5>सांसद- गोरखपुर </h5>
+              </span>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-4">
+            <div class="box" data-aos="fade-up">
+              <img src="assets/img/yogiimg/yogismall.jpg" style="max-width: 140px;" class="img-fluid" alt="">
+              <span>
+                <h4>2017-</h4>
+              </span>
+              <span>
+                <h5>सांसद- गोरखपुर </h5>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include('footer.php') ?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
