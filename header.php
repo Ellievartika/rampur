d831e44a577e3837a3ca14fa2f86aa96edbebfe0
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 17:02:28 +0530

    files

---
+++ header.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+</head>
+
+<body>
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" data-aos="fade-down" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center" data-aos="fade-left"><a href="mailto:office.bjpbijnor@gmail.com">office.bjpsambhal@gmail.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4" data-aos="fade-left"><span>+91 111 0000 0000, 100 0000 111</span></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="https://www.facebook.com/" class="facebook"><i class="bi bi-facebook" data-aos="fade-left"></i></a>
+        <a href="https://twitter.com/" class="twitter"><i class="bi bi-twitter" data-aos="fade-left"></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center mb-0">
+    <div class="container-fluid mx-3 d-flex justify-content-between">
+      <div class="logo">
+        <a href="index.php"><img src="assets/img/logo/rampurlogo.jpeg" alt=""></a>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.php"><span>होम</span></a></li>
+          <li class="dropdown"><a href=""><span>पार्टी के बारे में</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="hamarivichardhara.php">हमारी विचारधारा</a></li>
+              <li><a href="ekatamak.php">एकात्म मानववाद</a></li>
+              <li><a href="history.php">इतिहास</a></li>
+              <li><a href="antyodya.php">अंत्योदय</a></li>
+            </ul>
+          </li>
+
+          <li class="dropdown netratva"><a href=""><span>नेतृत्व</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li class="dropdown"><a href=""><span>संस्थापक</span></a>
+                <ul>
+                  <li><a href="shyamaprasad.php">डॉ. श्यामा प्रसाद मुखर्जी</a></li>
+                  <li><a href="panditdindyal.php">पंडित दीनदयाल उपाध्याय</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href=""><span>हमारे नेतृत्व</span></a>
+                <ul>
+                  <li><a href="atalbihari.php">श्री अटल बिहारी वाजपेयी </a></li>
+                  <li><a href="lalkrishna.php">श्री लाल कृष्ण आडवाणी</a></li>
+                  <li><a href="narendra.php">श्री नरेन्द्र मोदी </a></li>
+                  <li><a href="amitshah.php">श्री अमित शाह </a></li>
+                  <li><a href="jagatprakash.php">श्री जगत प्रकाश नड्डा </a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li><a href="adhyaksha.php"><span>अध्यक्ष</span></a></li>
+          <li class="dropdown"><a href=""><span>संगठन</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <!-- <li><a href="pradeshupadhyaksha.php">प्रदेश उपाध्य्क्ष</a></li> -->
+              <!-- <li><a href="pradeshmahamantri.php">प्रदेश महामंत्री</a></li> -->
+              <!-- <li><a href="pradeshmantri.php">प्रदेश मंत्री</a></li> -->
+              <li><a href="jilaupadhyaksha.php">जिला उपाध्य्क्ष</a></li>
+              <li><a href="">जिला महामंत्री</a></li>
+              <li><a href="">जिला मंत्री</a></li>
+              <li><a href="">जिला अध्यक्ष</a></li>
+              <li><a href="">जिला कोषाध्यक्ष</a></li>
+              <li><a href="">मोर्चा-अध्यक्ष</a></li>
+              <li class="dropdown"><a href=""><span>मंडल</span></a>
+                <ul>
+                  <li><a href="">मंडल अध्यक्ष</a></li>
+                  <!-- <li><a href=""></a></li> -->
+                </ul>
+              </li>
+              <li><a href="">आई.टी.एव. सोशल मीडिया विभाग</a></li>
+              <!-- <li><a href="media-prabhari.php">मीडिया प्रभारी </a></li> -->
+              <li><a href="">जिला मीडिया, सह मीडिया</a></li>
+              <li><a href="">कार्यालय संपर्क</a></li>
+            </ul>
+          </li>
+
+          <li class="dropdown"><a href=""><span>मंत्री मंडल</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="chiefminister.php">मुख्यमंत्री</a></li>
+              <li><a href="uppmukhyamantri.php">उप मुख्यमंत्री</a></li>
+            </ul>
+          </li>
+
+          <li class="dropdown"><a href=""><span>जन प्रतिनिधि</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="loksabhasansad.php">लोक सभा सांसद</a></li>
+              <li><a href="rajyasabhasansad.php">राज्यसभा सांसद</a></li>
+            </ul>
+          </li>
+
+          <li><a href="kamaljyotipatrika.php"><span>कमल ज्योति पत्रिका</span></a>
+          </li>
+
+          <li class="dropdown"><a href=""><span>सेवा ही संगठन : E-बुक</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="ebookpashchhimchetra.php"> E-बुक : पश्चिम क्षेत्र </a></li>
+              <li><a href="ebookbrazchetra.php"> E-बुक : ब्रज क्षेत्र </a></li>
+              <li><a href="ebookkanpurchetra.php"> E-बुक : कानपुर क्षेत्र </a></li>
+              <li><a href="ebookavadhchetra.php"> E-बुक : अवध क्षेत्र </a></li>
+              <li><a href="ebookkashichetra.php"> E-बुक : काशी क्षेत्र </a></li>
+              <li><a href="ebookgorakhpurchetra.php"> E-बुक : गोरखपुर क्षेत्र </a></li>
+            </ul>
+          </li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- navbar -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Breadcrumbs ======= -->
+  <section id="breadcrumbs" data-aos="fade-up" class="breadcrumbs">
+    <div class="container">
+      <div class="d-flex justify-content-between align-items-center">
+      </div>
+    </div>
+  </section><!-- End Breadcrumbs -->
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+</body>
+
+</html>
