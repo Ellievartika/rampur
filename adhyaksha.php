17f854247d0f0dad75b0d5b05c63ec90f7da40e5
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:51:43 +0530

    adhyakshafiles

---
+++ adhyaksha.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>अध्यक्ष - भारतीय जनता पार्टी</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+    <main id="main">
+        <!-- ======= Section ======= -->
+        <section id="pricing" class="pricing">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="box" data-aos="fade-up">
+                            <img src="assets/img/adhyaksha/sds.webp" class="img-fluid" alt="">
+                            <div class="btn-wrap" data-aos="fade-up">
+                                <a href="https://www.facebook.com/SwatantraDevSingh/" class="btn-icon"><i
+                                        class="fab fa-facebook-square mx-2"></i></a>
+                                <a href="https://twitter.com/swatantrabjp" class="btn-icon"><i
+                                        class="fab fa-twitter-square mx-2"></i></a>
+                            </div>
+                            <div class="btn-wrap" data-aos="fade-up">
+                                <a href="http://yogiadityanath.in/" class="btn-buy">http://www.yogiadityanath.in</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <div class="box" data-aos="fade-down">
+                            <div data-aos="fade-down">
+                                <h1>श्री स्‍वतंत्र देव सिंह</h1>
+                            </div>
+                            <br>
+                            <div style="text-align: right;">
+                                <h5>प्रदेश अध्यक्ष – भारतीय जनता पार्टी, उत्तर प्रदेश </h5>
+                            </div>
+                            <br>
+                            <p style="text-align: justify;">
+                                मा० श्री स्वतंत्र देव सिंह का जन्म मिर्जापुर जिले में 13 फरवरी 1964 को हुआ था। इनकी मां
+                                का नाम श्रीमती रामा देवी और पिता का नाम श्री अल्लर सिंह है। मिर्जापुर जनपद में जन्मे
+                                श्री स्वतंत्र देव सिंह जी ने बुन्देलखंड के जालौन को अपनी कर्मभूमि बनाई। यहीं से राजनीति
+                                की शुरुआत करते हुए इन्होंने प्रदेश की राजनीति में अपनी एक खास पहचान बनाई। बिना किसी
+                                राजनीतिक पृष्ठभूमि वाले परिवार में जन्मे श्री स्वतंत्रदेव सिंह जी का सियासत में कदम रखने
+                                वाले अपने परिवार के पहले व्यक्ति हैं। जो आरएसएस से जुड़कर वर्तमान में बीजेपी जैसी
+                                राजनीतिक पार्टी के माध्यम से प्रदेश की राजनीति में सक्रिय भूमिका निभा रहे हैं।
+                            </p>
+                            <ul style="text-align: left;" data-aos="fade-up">
+                                <li>
+                                    <a href="">श्री स्वतंत्र देव सिंह का सियासी सफर</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">1986- राष्ट्रीय स्वयं सेवक संघ से जुड़े</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">1988- अखिल भारतीय विद्यार्थी परिषद के संगठन मंत्री</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">1991- भारतीय जनता युवा मोर्चा के कानपुर के प्रभारी</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">1994- बुन्देलखण्ड क्षेत्र के युवा मोर्चा प्रभारी</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">1996 व 1998- भारतीय जनता युवा मोर्चा प्रदेश महामंत्री</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2001- भारतीय जनता युवा मोर्चा प्रदेश अध्‍यक्ष</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2004- विधान परिषद सदस्य</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2004 से 2010- प्रदेश महामंत्री</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2010 से 2012- प्रदेश उपाध्यक्ष</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2012 से 2017- प्रदेश महामंत्री</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2013-पश्चिम क्षेत्र प्रभारी</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2014- प्रदेश सदस्यता प्रमुख</a>
+                                </li>
+                                <br>
+                                <li>
+                                    <a href="">2017- प्रदेश सरकार में राज्‍य मंत्री (स्‍वतंत्र प्रभार) परिवहन एवं
+                                        प्रोटोकॉल व राज्‍य मंत्री बिजली विभाग का दायित्‍व</a>
+                                </li>
+                            </ul>
+                            <div class="btn-wrap" style="text-align: left;"data-aos="fade-left">
+                                <a href="" class="btn-buy">जन्म की तारीख - 13/02/1964</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- End Section -->
+    </main><!-- End #main -->
+    <!-- ======= Footer ======= -->
+    <?php include('footer.php') ?>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
