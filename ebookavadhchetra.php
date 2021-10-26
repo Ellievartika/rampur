893126ec314f5792443a2dd086c44873d8095916
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:56:14 +0530

    files

---
+++ ebookavadhchetra.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>E-बुक : अवध क्षेत्र - भारतीय जनता पार्टी</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <main id="main">
+        <!-- ======= Section ======= -->
+        <section id="pricing" class="pricing">
+            <div class="container-fluid">
+
+                <div class="text-center mb-4" data-aos="fade-down" style="background-color: #f3f1f0; height: 40px;">
+                    <div class="kamaljyotiheading">
+                        <h2>
+                            E-बुक : अवध क्षेत्र
+                        </h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    अम्‍बेडकर नगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Ambedkar_Nagar-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    अयोध्‍या जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/0001-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    अयोध्‍या महानगर
+
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Ayodhya_Mahanagar-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बहराइच
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Bahraich-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बलरामपुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_BalramPur-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    गोंडा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Gonda-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    हरदोई
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Hardoi-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    लखीमपुर खीरी
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Lakhimpur-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    लखनऊ जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Lucknow_JIla-01-212x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    लखनऊ महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Lucknow_Mahanagar-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    रायबरेली
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_RaeBareilly-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    सीतापुर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Sitapur-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    उन्‍नाव
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Unnao-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बाराबंकी
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Barabanki-01-212x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    श्रावस्‍ती
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookavadhchetra/E-Book_Shrawasti-01-208x300.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- End Section -->
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include('footer.php') ?>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
