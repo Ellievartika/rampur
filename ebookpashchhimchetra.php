bc12708a0012cba84516477db6fdee476e74784e
Ellievartika <ellievartika@zoyoecommerce.com>
2021-10-26 16:59:54 +0530

    files

---
+++ ebookpashchhimchetra.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>E-बुक : पश्चिम क्षेत्र - भारतीय जनता पार्टी</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php include('header.php') ?>
+
+    <main id="main">
+        <!-- ======= Section ======= -->
+        <section id="pricing" class="pricing">
+            <div class="container-fluid">
+
+                <div class="text-center mb-4" data-aos="fade-down" style="background-color: #f3f1f0; height: 40px;">
+                    <div class="kamaljyotiheading">
+                        <h2>
+                            E-बुक : पश्चिम क्षेत्र
+                        </h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    अमरोहा
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/AMROHA_Page_01-210x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बागपत
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/BAGPAT_Page_01-212x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बुलंदशहर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/BULANDSHER_Page_01-211x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    गौतमबुद्धनगर जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/GAUTAMBUDH-NAGAR_Page_01-213x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    गाजियाबाद महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/GAZIABAD-MHANAGAR_Page_01-215x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    गाजियाबाद जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/GHAZIABAD-JILA_Page_01-213x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    हापुड़
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/HAPUR-JILA_Page_01-209x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मेरठ जिला 
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/MEERUT-JILA_Page_01-208x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मेरठ महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/MEERUT-MHANAGAR_Page_01-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मुजफ्फरनगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/MUJAFFAR-NAGAR_Page_01-211x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मुरादाबाद जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/MURADABAD-JILA_Page_01-211x300.jpg" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मुरादाबाद महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/MURADABAD-MHANAGAR_Page_01-215x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    नोएडा महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/Noida-Mahanagar_Page_01-215x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    रामपुर जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/rampur_Page_01-208x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    सहारनपुर जिला
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/SAHARANPUR-JILA_Page_01-210x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मुसहारनपुर महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/SAHARANPUR-MAHNAGAR_Page_01-210x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    सम्‍भल
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/Sambhal_Page_01-212x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    शामली
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/SAMLI_Page_01-210x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    बिजनौर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/BIJNOR_Page_01-1-212x300.webp" class="img-fluid"
+                                alt="">
+                        </div>
+                    </div>
+                    <div class="col-lg-3 mb-3" data-aos="fade-up">
+                        <div class="box">
+                            <div class="kamalheading">
+                                <h3>
+                                    मुरादाबाद महानगर
+                                </h3>
+                            </div>
+                            <img src="assets/img/ebookpaschimchetra/MURADABAD-MHANAGAR_Page_01-215x300.webp"
+                                class="img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section><!-- End Section -->
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <?php include('footer.php') ?>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
